2c16f1416e5c92d20d51057b4e7caada1361aec8
Christopher Hallett <chrisjameshal@gmail.com>
2022-02-09 10:29:42 +0000

    Add onLoad effect to fit view and switched to js

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import "./App.css";
-import ReactFlow, { MiniMap, Controls, Background } from "react-flow-renderer";
-import { initialElements } from "./initialElements";
+import MainFlow from "./components/MainFlow";
 
 function App() {
-  //const [elements, setElements] = useState(initialElements);
   return (
     <div className="App">
-      <ReactFlow
-        elements={initialElements}
-        snapToGrid={true}
-        key="edge-with-button"
-        style={{ width: " 100vw ", height: "100vh" }}
-      >
-        <MiniMap />
-        <Controls />
-        <Background />
-      </ReactFlow>
+      <MainFlow />
     </div>
   );
 }
